85273865020c1ed976db9d85449be829c41b17eb
Shakirov Marsel <imanager666@gmail.com>
2026-01-22 11:30:59 +0300

    improve icon.jsx

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -1,16 +1,16 @@
 const iconModules = import.meta.glob('./icons/*.jsx', { eager: true })
-const icons = {}
-Object.keys(iconModules).forEach((path) => {
+
+const icons = Object.entries(iconModules).reduce((icons, [path, module]) => {
 	const componentName = path.split('/').pop().replace('.jsx', '')
-	icons[componentName] =
-		iconModules[path].default || iconModules[path][componentName]
-})
+	icons[componentName] = module.default || module[componentName]
+	return icons
+}, {})
 
 export const Icon = ({ name, size = 24, color = 'currentColor', ...props }) => {
 	const IconComponent = icons[name]
 
 	if (!IconComponent) {
-		console.warn(`Icon '${name}' not found`)
+		console.warn(`Icon '${name}' not found in available icons`)
 		return null
 	}
 
